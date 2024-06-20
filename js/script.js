02353023dbc768283096664b9eb9dac27a69d097
web-site-link <web.teacher.tanya@gmail.com>
2024-06-20 16:49:40 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let humbs = document.querySelectorAll('.humb')
+let links = document.querySelectorAll('nav a')
 let header = document.querySelector('header')
 
 ;[...humbs].map((el) =>
@@ -6,3 +7,23 @@ let header = document.querySelector('header')
     header.classList.toggle('active')
   })
 )
+;[...links].map((el) =>
+  el.addEventListener('click', function (e) {
+    header.classList.remove('active')
+  })
+)
+
+const anchors = document.querySelectorAll('nav a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+    })
+  })
+}
