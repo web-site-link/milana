4894e3b379ec73e81271323c6ce31c06d048fb45
web-site-link <web.teacher.tanya@gmail.com>
2024-06-22 15:48:58 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -27,3 +27,30 @@ for (let anchor of anchors) {
     })
   })
 }
+
+// Gallery modal
+var modalVisible = false
+var $modal = $('.modal')
+
+$('.gallery img').click(function (e) {
+  let loc = $(this).data('loc')
+  $modal.html('<img src=' + loc + " class='modal-img'>")
+  $modal.fadeIn(200)
+  modalVisible = true
+})
+
+$('.modal').click(function (e) {
+  let $img = $('.modal-img').get(0)
+  if (e.target != $img && modalVisible) {
+    $modal.fadeOut(200)
+    modalVisible = false
+  }
+})
+
+// Video modal
+$('.js-video-button').modalVideo({
+  youtube: {
+    controls: 0,
+    nocookie: true,
+  },
+})
